3b58c39f3c68814c4afca1cb63b579faa16b4810
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-07-19 01:13:40 +0900

    42. A Closer Look At JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -25,6 +26,14 @@ function App() {
   // const para = document.createElement('p');
   // para.textContent = 'This is also visible';
   // document.getElementById('root').append(para);
+
+  // return React.createElement(
+  //   'div', 
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
