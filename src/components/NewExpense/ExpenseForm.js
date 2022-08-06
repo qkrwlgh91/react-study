5e478275db93d7d5b5390caea1e98b3ae80644e0
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-08-06 21:59:49 +0900

    58. Adding Two-Way Binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -57,7 +57,9 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData);
-        // setEnteredTitle('');
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
@@ -67,16 +69,29 @@ const ExpenseForm = () => {
                     <label>Title</label>
                     <input 
                         type='text'
+                        value={enteredTitle}
                         onChange={titleChangeHandler} 
                     />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                    <input 
+                        type='number' 
+                        min='0.01' 
+                        step='0.01' 
+                        value={enteredAmount} 
+                        onChange={amountChangeHandler} 
+                    />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2022-01-01' max='2023-12-31' onChange={dateChangeHandler} />
+                    <input 
+                        type='date' 
+                        min='2022-01-01' 
+                        max='2023-12-31' 
+                        value={enteredDate} 
+                        onChange={dateChangeHandler} 
+                    />
                 </div>
             </div>
             <div className='new-expense__actions'>
