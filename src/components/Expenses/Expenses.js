04a24d71e1aa1029fc89513a0cccafc4ed728bf3
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-08-17 23:29:17 +0900

    67. Outputting Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,21 +18,49 @@ function Expenses(props) {
         (expense) => {
             return expense.date.getFullYear().toString() === filteredYear
         }
-    )
+    );
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />)
+        )
+    };
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 {/* {[<Card />, <Card />]} */}
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />)
-                )}
+                {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+                {filteredExpenses.length > 0 && 
+                    filteredExpenses.map((expense) => (
+                        <ExpenseItem 
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />)
+                    )} */}
+                    {expensesContent}
+                {/* {filteredExpenses.length === 0 ? (
+                    <p>No expenses found.</p>
+                ) : (
+                    filteredExpenses.map((expense) => (
+                        <ExpenseItem 
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />)
+                    )
+                )} */}
             </Card>
         </div>
     )
