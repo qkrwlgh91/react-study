dd3f4171bebefea7712c0b0e372919d308bd95d2
qkrwlgh91 <59594319+qkrwlgh91@users.noreply.github.com>
2022-08-19 00:53:35 +0900

    68. Adding Conditional Return Statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -15,15 +15,17 @@ function ExpenseItem(props) {
     };
     
     return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-            {/* <button onClick={() => { console.log('Clicked!') }}>Change Title</button> */}
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{title}</h2>
+                    <div className='expense-item__price'>${props.amount}</div>
+                </div>
+                {/* <button onClick={() => { console.log('Clicked!') }}>Change Title</button> */}
+                <button onClick={clickHandler}>Change Title</button>
+            </Card>
+        </li>
     )
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 function Expenses(props) {
@@ -20,19 +20,6 @@ function Expenses(props) {
         }
     );
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem 
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />)
-        )
-    };
-
     return (
         <div>
             <Card className="expenses">
@@ -48,7 +35,7 @@ function Expenses(props) {
                             date={expense.date}
                         />)
                     )} */}
-                    {expensesContent}
+                    {/* {expensesContent} */}
                 {/* {filteredExpenses.length === 0 ? (
                     <p>No expenses found.</p>
                 ) : (
@@ -61,6 +48,7 @@ function Expenses(props) {
                         />)
                     )
                 )} */}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
